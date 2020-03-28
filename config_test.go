1dec6c49e358f906386c6609e525f02623f28915
Richard Bowden <rb@rdbg.co.uk>
2020-03-28 13:04:05 +0800

    testing cmd line args
    this is a pain due to flags being a singleton, one time use, it does not expose a way to reset the default flag set

--- config_test.go
+++ config_test.go
@@ -1,74 +1,100 @@
 package main
 
-import (
-	"os"
-	"testing"
-)
+// func TestTagDoubleStringWithAdditionalFlags(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p server1 ss -- -A -4 uptime
+// 	args := []string{
+// 		"--debug",
+// 		"-r",
+// 		"ap-southeast-1",
+// 		"-p",
+// 		"server1",
+// 		"ss",
+// 		"--",
+// 		"-A",
+// 		"-4",
+// 		"uptime",
+// 	}
 
-func TestCLIBasic(t *testing.T) {
-	os.Args = append(os.Args, "")
-}
+// 	os.Args = args
+// 	_, err := getESSHConfig()
 
-func TestTagDoubleStringWithAdditionalFlags(t *testing.T) {
-	// --debug -r ap-southeast-1 -p server1 ss -- -A -4 uptime
-}
+// 	if err.Error() != "only specifiy an instance id or a tag, if a tag has a space, wrap in double quotes" {
+// 		t.Error("failed to detect DoubleString error")
+// 	}
+// }
 
-func TestTagDoubleString(t *testing.T) {
-	// --debug -r ap-southeast-1 -p server1 ss
-}
+// func TestTagDoubleString(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p server1 ss
+// 	args := []string{
+// 		"--debug",
+// 		"-r",
+// 		"ap-southeast-1",
+// 		"-p",
+// 		"server1",
+// 		"ss",
+// 	}
 
-func TestTagDoubleStringWithAdditionalArgsButNoDoubleDash(t *testing.T) {
-	// --debug -r ap-southeast-1 -p server1 ddd -A -4 uptime
-}
+// 	os.Args = args
 
-func TestTagSingleTagWithAdditionalFlagsButNoDoubleDash(t *testing.T) {
-	// --debug -r ap-southeast-1 -p server1 -A -4 uptime
-}
+// 	_, err := getESSHConfig()
+// 	fmt.Println(err)
+// 	if err.Error() != "only specifiy an instance id or a tag, if a tag has a space, wrap in double quotes" {
+// 		t.Error("failed to detect DoubleString error")
+// 	}
+// }
 
-func TestInstIDDoubleStringWithAdditionalFlags(t *testing.T) {
-	// --debug -r ap-southeast-1 -p i-xxxx ss -- -A -4 uptime
-}
+// func TestTagDoubleStringWithAdditionalArgsButNoDoubleDash(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p server1 ddd -A -4 uptime
+// }
 
-func TestInstIDDoubleString(t *testing.T) {
-	// --debug -r ap-southeast-1 -p i-xxxx ss
-}
+// func TestTagSingleTagWithAdditionalFlagsButNoDoubleDash(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p server1 -A -4 uptime
+// }
 
-func TestInstIDWithDoubleStringWithAdditionalFlagsNoDoubleDash(t *testing.T) {
-	// --debug -r ap-southeast-1 -p i-xxxx dd -A -4 uptime
-}
+// func TestInstIDDoubleStringWithAdditionalFlags(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p i-xxxx ss -- -A -4 uptime
+// }
 
-func TestNoInstIDOrTagWithAdditionalFlagsNoDoubleDash(t *testing.T) {
-	// --debug -r ap-southeast-1 -p -A -4 uptime
-}
+// func TestInstIDDoubleString(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p i-xxxx ss
+// }
 
-func TestTagCorrectTagAndAdditionalFlags(t *testing.T) {
-	// --debug -r ap-southeast-1 -p server1 -- -A -4 uptime
-}
+// func TestInstIDWithDoubleStringWithAdditionalFlagsNoDoubleDash(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p i-xxxx dd -A -4 uptime
+// }
 
-func TestCTagCorrectTag(t *testing.T) {
-	// --debug -r ap-southeast-1 -p server1
-}
+// func TestNoInstIDOrTagWithAdditionalFlagsNoDoubleDash(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p -A -4 uptime
+// }
 
-func TestTagCorrectQuotedTag(t *testing.T) {
-	// --debug -r ap-southeast-1 -p "server 1"
-}
+// func TestTagCorrectTagAndAdditionalFlags(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p server1 -- -A -4 uptime
+// }
 
-func TestTagCorrectQuotedTagWithAdditionalFlags(t *testing.T) {
-	// --debug -r ap-southeast-1 -p "server 1" -- -A -4 uptime
-}
+// func TestCTagCorrectTag(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p server1
+// }
 
-func TestInstIDCorrectWithAdditionalFlags(t *testing.T) {
-	// --debug -r ap-southeast-1 -p i-xxxxx -- -A -4 uptime
-}
+// func TestTagCorrectQuotedTag(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p "server 1"
+// }
 
-func TestInstIDCorrectJustID(t *testing.T) {
-	// --debug -r ap-southeast-1 -p i-xxxxx
-}
+// func TestTagCorrectQuotedTagWithAdditionalFlags(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p "server 1" -- -A -4 uptime
+// }
 
-func TestCorrectNoTagNoInstID(t *testing.T) {
-	// --debug -r ap-southeast-1 -p
-}
+// func TestInstIDCorrectWithAdditionalFlags(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p i-xxxxx -- -A -4 uptime
+// }
 
-func TestCorrectNoTagNoInstIDWithAdditionalFlags(t *testing.T) {
-	// --debug -r ap-southeast-1 -p -- -A -4 uptime
-}
+// func TestInstIDCorrectJustID(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p i-xxxxx
+// }
+
+// func TestCorrectNoTagNoInstID(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p
+// }
+
+// func TestCorrectNoTagNoInstIDWithAdditionalFlags(t *testing.T) {
+// 	// --debug -r ap-southeast-1 -p -- -A -4 uptime
+// }
